587543c0f591308000ccbb5b85134d8a2f5e6896
Eyob Tariku <eyobtariku@gmail.com>
2018-04-16 09:48:36 +0300

    Create EMAIL_BACKEND enviromental variable to settings.py

--- core/settings.py
+++ core/settings.py
@@ -162,3 +162,8 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+EMAIL_BACKEND = config(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.smtp.EmailBackend'
+)
