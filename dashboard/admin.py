fbd74a6ac1573ad2da48f53b7f11ead71784afcc
Eyob Tariku <eyobtariku@gmail.com>
2018-04-17 14:10:46 +0300

    Register Schedule model to admin site

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -29,6 +29,9 @@ class ConsultantAdmin(admin.ModelAdmin):
     search_fields = ('full_name', 'short_name')
 
 
+admin.site.register(models.Activity)
+
+
 @admin.register(models.Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('short_name', 'consultant', 'construction_type', 'status',)
@@ -57,3 +60,15 @@ class UserNotificationAdmin(admin.ModelAdmin):
     list_display = ('notify_to', 'notification', 'is_seen', 'is_email_sent')
     list_filter = ('is_seen', 'is_email_sent')
     search_fields = ('notification', )
+
+
+@admin.register(models.Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('project', 'title', 'period', 'is_active', 'updated_at', )
+    list_filter = ('is_active', 'project', )
+    search_fields = ('title', )
+
+
+@admin.register(models.Plan)
+class PlanAdmin(admin.ModelAdmin):
+    pass
