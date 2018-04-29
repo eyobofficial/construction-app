3b57536292ab11eaac6dcefbc520d5971d15b3c3
eyob tariku <eyobtariku@gmail.com>
2018-04-29 15:10:46 +0300

    Install django-extensions & updated requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django_extensions',
     'form_utils',
     'crispy_forms',
     'widget_tweaks',
